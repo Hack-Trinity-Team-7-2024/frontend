817fb96099f5ec17fa8c920dec9c5ecb136ad776
farushed <farushed@tcd.ie>
2024-02-24 13:58:39 +0000

    Fetch tasks to and from backend

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,20 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import TypingPopup from '../Components/TypingPopup';
 import TaskList from '../Components/TaskList';
-
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
 
+  useEffect(() => {
+    fetch('/api/tasks')
+      .then(res => res.json())
+      .then(j => setTasks(j));
+  }, []); // only run once
 
   const addTask = (taskText) => {
     let task = {
-      id: tasks.length + 1,
+      // id: tasks.length + 1,
       text: taskText,
-      completed: false
-    }
-    console.log(task);
-    setTasks([...tasks, task]);
+      completed: true
+    };
+
+    fetch('/api/tasks',
+      {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(task)
+      }
+    ).then(
+      res => res.json()
+    ).then(
+      j => {
+        let actualTask = {
+          ...task,
+          id: j.id
+        };
+        console.log(actualTask);
+        setTasks([...tasks, actualTask]);
+      }
+    );
+
+
   };
 
   // const toggleTask = (id) => {
