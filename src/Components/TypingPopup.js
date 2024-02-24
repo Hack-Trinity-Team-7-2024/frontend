10dedbbfeb507cc507555272a1a01f9d5b72013b
farushed <farushed@tcd.ie>
2024-02-24 11:34:52 +0000

    Remove popup functionality

--- src/Components/TypingPopup.js
+++ src/Components/TypingPopup.js
@@ -4,31 +4,8 @@ import '../Styles/TypingPopup.css';
 import Task from './Task';
 
 const TypingPopup = () => {
-  const [showPopup, setShowPopup] = useState(false);
   const [newTask, setNewTask] = useState('');
   const [tasks, setTasks] = useState([]);
-  useEffect(() => {
-    // only register the handler etc if the popup isn't shown
-    if (!showPopup) {
-      const handleTyping = (event) => {
-        // Check if nothing is currently focused
-        if (document.activeElement === document.body) {
-          // Check if the pressed key corresponds to a printable character
-          const charCode = event.key;
-          if (charCode.length === 1 && charCode !== ' ') {
-              setShowPopup(true);
-          }
-        }
-      };
-  
-      document.body.addEventListener('keydown', handleTyping);
-  
-      return () => {
-        document.body.removeEventListener('keydown', handleTyping);
-      };
-    }
-  }, [showPopup]);
-
 
   const handleTaskChange = (event) => {
     setNewTask(event.target.value);
@@ -42,29 +19,26 @@ const TypingPopup = () => {
     event.preventDefault();
     addTask(newTask);
     setNewTask('');
-    setShowPopup(false);
   }
 
   return (
     <div className='typing-popup-container'>
-      {showPopup && (
-          <form onSubmit={handleSubmit}>
-            <TextField
-              id='outlined-textarea'
-              label='New Task'
-              placeholder='Start Typing!'
-              // multiline
-              autoFocus
-              variant='filled'
-              style={{width:'30vw'}}
-              value={newTask}
-              onChange={handleTaskChange}
-            />
-            <Button variant="contained" type="submit" style={{ marginTop: '10px' }}>
-              Add Task
-            </Button>
-          </form>
-      )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          id='outlined-textarea'
+          label='New Task'
+          placeholder='Start Typing!'
+          // multiline
+          autoFocus
+          variant='filled'
+          style={{width:'30vw'}}
+          value={newTask}
+          onChange={handleTaskChange}
+        />
+        <Button variant="contained" type="submit" style={{ marginTop: '10px' }}>
+          Add Task
+        </Button>
+      </form>
       
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         {tasks.map((task, index) => (
